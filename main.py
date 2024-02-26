99d6ffa4057a27784a8eee8af797ca7ae2113e90
Caio de Araújo <126365047+caiotdearaujo@users.noreply.github.com>
2024-02-26 13:22:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+amarelinha = ([0], [1], [2], [3, 4], [5], [6], [7, 8], [9], [10])
+rodada = 1
+indice = 0
+pes_em = amarelinha[indice]
+
+
+def print_local(local):
+
+  if pes_em == [0]:
+    print("Você está no início da amarelinha")
+  elif pes_em == [10]:
+    print("Você está com dois pés no céu")
+    print("Você se virou")
+  elif len(local) == 2:
+    print("Você pisou com dois pés em", local[0], "e", local[1])
+  else:
+    print("Você pisou com um pé em", local[0])
+
+
+while rodada < 10:
+
+  pedra_em = rodada
+  print("A pedra foi jogada na casa ", pedra_em)
+
+  while pes_em != [10]:
+    print_local(pes_em)
+    if pedra_em in amarelinha[indice + 1]:
+      if len(amarelinha[indice + 1]) == 2:
+        indice += 1
+        pes_em = amarelinha[indice]
+        pes_em.pop(pes_em.index(pedra_em))
+      else:
+        indice += 2
+        pes_em = amarelinha[indice]
+    else:
+      indice += 1
+      pes_em = amarelinha[indice]
+
+  while pes_em != [0]:
+    print_local(pes_em)
+    if pedra_em in amarelinha[indice - 1]:
+      print("Você pegou a pedra")
+      if len(amarelinha[indice - 1]) == 2:
+        indice -= 1
+        pes_em = amarelinha[indice]
+        pes_em.pop(pes_em.index(pedra_em))
+      else:
+        indice -= 2
+        pes_em = amarelinha[indice]
+    else:
+      indice -= 1
+      pes_em = amarelinha[indice]
+
+  rodada += 1
+  print()
+
+print("Você venceu a amarelinha")
